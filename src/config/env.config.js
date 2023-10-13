de74becc5be43581a4db7b135a069159b0643052
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2023-10-13 16:43:09 +0700

    Initiate inedx.js

---
+++ src/config/env.config.js
@@ -0,0 +1,17 @@
+require("dotenv/config");
+
+const dictionary = {
+    // SECRET_KEY: process.env.SECRET_KEY,
+    HOST: process.env.HOST,
+    // DB_USER: process.env.DB_USER,
+    // DB_PASSWORD: process.env.DB_PASSWORD,
+    // DB_HOST: process.env.DB_HOST,
+    // DB_NAME: process.env.DB_NAME,
+    // CLIENT_KEY: process.env.CLIENT_KEY,
+    // SERVER_KEY: process.env.SERVER_KEY,
+    // PREFIX: process.env.PREFIX
+};
+
+module.exports = function env(key) {
+    return dictionary[key];
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const env = require("./config/env.config");
+const cors = require("cors");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+        methods: "GET, HEAD, PUT, PATCH, POST, DELETE"
+    })
+);
+
+// app.use('/api', apiRouter);
+
+app.all('*', (req, res) => {
+    return res.status(404).json({ message: `Page Not Found!` });
+});
+
+const port = 3000;
+app.listen(port, env("HOST"), () => console.log(`Listening on port ${port}!`));
